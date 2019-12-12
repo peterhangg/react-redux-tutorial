be4d45f79d9a7c136e2e007c4168a89dafb17617
Peter Hang <peterhanggg@gmail.com>
2019-12-11 20:38:18 -0500

    refactored reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,13 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
 const initialState = {
   articles: []
 };
 
 export default function rootReducer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE) {
+    return {...state, articles: [...state.articles, action.payload]};
+  }
   return state;
 };
 
