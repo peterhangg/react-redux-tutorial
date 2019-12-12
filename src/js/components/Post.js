4f2cf2700bcecb3043dc19985321da1ac01ca7d3
Peter Hang <peterhanggg@gmail.com>
2019-12-11 23:50:29 -0500

    refactor with redux thunk and added Post component to app.js

--- src/js/components/Post.js
+++ src/js/components/Post.js
@@ -2,17 +2,24 @@ import React, { useEffect } from 'react';
 import { connect } from "react-redux";
 import { getData } from "../actions/index";
 
-export function Post(props) {
+const mapStateToProps = state => {
+  return {
+    articles: state.remoteArticles.slice(0, 10)
+  }
+};
 
+function Post(props) {
   useEffect(() => {
     props.getData();
-  },[props]);
-  
+  }, [props]);
+
   return (
-    <div>
-      
-    </div>
-  )
+    <ul>
+      {props.articles.map(article => (
+        <li key={article.id}>{article.title}</li>
+      ))}
+    </ul>
+  );
 }
 
-export default connect(null, { getData })(Post);
+export default connect(mapStateToProps, {getData}) (Post);
