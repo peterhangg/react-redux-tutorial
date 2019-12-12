391b6096be6bf78d393604efbf98afdf31c1cf00
Peter Hang <peterhanggg@gmail.com>
2019-12-11 23:59:58 -0500

    refactor getData fetch with axios

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,4 +1,5 @@
 import { ADD_ARTICLE } from "../constants/action-types";
+import axios from "axios";
 
 export function addArticle(payload) {
   return { type: ADD_ARTICLE, payload };
@@ -6,10 +7,9 @@ export function addArticle(payload) {
 
 export function getData() {
   return function(dispatch) {
-    return fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => response.json())
-      .then(json => {
-        dispatch({ type: "DATA_LOADED", payload: json });
+    return axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => {
+        dispatch({ type: "DATA_LOADED", payload: res.data });
       });
   };
 };
